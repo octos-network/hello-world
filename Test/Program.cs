61c929f181d58d4dd36046fe97d377afb7e05992
Benedikt Conrad <ConradBe@Admins-MBP.fritz.box>
2020-08-18 10:22:25 +0200

    test added

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numberGen = new Random();
+
+            int roll = 0;
+            int attempts = 0;
+
+            while(roll != 80) {
+                roll = numberGen.Next(1, 81);
+                Console.WriteLine("You rolled: " + roll);
+                attempts++;
+            }
+
+            Console.WriteLine("It took you " + attempts + " attempts to roll a eighty");
+
+            // warten vor den schließen
+            Console.ReadKey();
+        }
+        
+    }
+}
